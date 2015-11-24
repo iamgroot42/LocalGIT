a6bcafb35211f3258c7b5d233b0ea2f3d89a6d93
Anshuman <anshuman14021@iiitd.ac.in>
2015-11-24 20:24:00 +0530

    Added validation for valid repo name.

--- NewDeadline.py
+++ NewDeadline.py
@@ -1,10 +1,11 @@
 import os
 import datetime
+import re
 # Extract course code from current folder
 # course=raw_input("Enter course code") 
 name=raw_input("Enter name of deadline\n")
+replaced=re.sub('[^A-Za-z0-9_.-]','-',name) #Regex for permitted repo names
 # print(os.getcwd())	
-name=name.replace(" ","-") #Repo names can't have spaces
 while True:
 	time=raw_input("Enter deadline for submission (DD MM YYYY HH MM)\n")
 	time=time.split(' ')
@@ -17,12 +18,15 @@ while True:
 
 fd=raw_input("Enter description for deadline \n")
 print("> Deadline  Details :\n")
+print("Deadline name : "+replaced)
 print("Deadline for submission : "+str(timex))
-print("Deadline description :")
-print(fd+"\n")
+print("Deadline description : ",fd)
 choice=raw_input("> Confirm deadline? (y/n)\n")
 if(choice=='y' or choice=='Y'):
-	# Add entry to file of deadlines,crontab
+	#Read from file of users	
+	#Create repository <name> for each user in the list by the name 
+	#Add pre-push hooks to each initialized repo
+	#Add compression script to crotab 
 	print("Deadline created!")
 else:
   print("Deadline not created")	
