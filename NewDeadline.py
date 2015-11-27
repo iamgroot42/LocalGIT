104976d8c528bf64ebd2c3b52fe551235f90a30b
Anshuman <anshuman14021@iiitd.ac.in>
2015-11-27 19:58:28 +0530

    NewDeadline.py almost ready for work.

--- NewDeadline.py
+++ NewDeadline.py
@@ -3,10 +3,19 @@ import datetime
 import requests
 import re
 import json
-# Extract course code from current folder
+import sys
+
+try:
+	port=sys.argv[1]
+except:
+	print "Too few arguments (Provide port)"
+	exit()
+
 # course=raw_input("Enter course code") 
-link=raw_input("Enter HTTP URL of deadline-repository\n")
-# Parse link to extract name of repo
+link=raw_input("Enter clone URL (HTTPS/SSH) of deadline-repository\n")
+#Extracting name of repo from link :
+reponame=link.split('/')[-1].split('.git')[0] 
+path=os.path.abspath(os.getcwd())
 
 while True:
 	time=raw_input("Enter deadline for submission (DD MM YYYY HH MM)\n")
@@ -19,19 +28,22 @@ while True:
 		print "Invalid date"
 
 print("> Deadline  Details :\n")
-print("  Deadline name : "+name)
+print("  Deadline name : "+reponame)
 print("  Deadline for submission : "+str(timex))
-print("  Deadline description : ",fd)
-choice=raw_input("> Confirm deadline? (y/n)\n")
+choice=raw_input("> Confirm deadline? (y/n) ")
 if(choice=='y' or choice=='Y'):
-	#Read from file of users	
-	#Create repository <name> for each user in the list by the name 
-	#Add pre-push hooks to each initialized repo
-	#Add compression script using at command : http://www.computerhope.com/unix/uat.htm
-	# For each user : 
-	params={"clone_addr":link,"uid":userid,"repo_name":reponame,"private":"true"}
-	r=requests.post("http://localhost:3000/api/v1/repos/migrate", data = params)
-	assert(r.status_code==201)
+	#Read from file of users
+	try:
+		f=open('students','r')
+	except:
+		print "File of students' list not found"
+		exit()
+	for line in f:
+		line=line.rstrip('\n')
+		userid=line.split(' ')[1]
+		#Add compression script using 'at' command : http://www.computerhope.com/unix/uat.htm
+		params={"clone_addr":link,"uid":userid,"repo_name":reponame,"private":"true"}
+		r=requests.post("http://localhost:"+port+"/api/v1/repos/migrate", data = params)
 	print("Deadline created!")
 else:
   print("Deadline not created")	
