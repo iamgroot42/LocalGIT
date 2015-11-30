3d8196cffcd72556898b113bd2df6e2e5afd73f5
Anshuman Suri <anshuman14021@iiitd.ac.in>
2015-12-01 02:35:43 +0530

    Update and rename Pre-Receive GitHook.py to githook.py
    Reject after deadline now works ^_^

--- githook.py
+++ githook.py
@@ -6,19 +6,24 @@ import datetime
 
 # Reject push if deadline expired
 curdir=os.getcwd().split(os.sep)
-rep_name=curdir[-2].split('.git')[0]
-wanted_dir=curdir[:len(curdir)-3]
+# print curdir
+rep_name=curdir[-1].split('.git')[0]
+# print rep_name
+wanted_dir=curdir[:len(curdir)-2]
+# print wanted_dir
 os.chdir(os.sep.join(wanted_dir))
+# print os.getcwd()
 rep_name+="_deadline"
+# print rep_name
 f=open(rep_name,'r')
 deadline=datetime.datetime.strptime(f.readline(),'%Y-%m-%d %H:%M')
 f.close()
 now=datetime.datetime.now()
 #Read deadline for this assignment from file
 if(now<deadline):
-	print"\nDeadline Passed\n"
+	print"\nDeadline Passed!\n"
 	sys.exit(1)
 
 # Read in each ref that the user is trying to update
 for line in fileinput.input():
-	print "\nPotato pushed ^_^\n"
+	print "\nPush successful!\n"
